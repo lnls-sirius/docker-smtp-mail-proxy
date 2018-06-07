c8178713e27274e6ee74fe81817fac515691631d
Aureo Freitas <aureo.carneiro@gmail.com>
2018-06-07 12:09:21 -0300

    uses container's interface ip address

--- scripts/start-mail-proxy.sh
+++ scripts/start-mail-proxy.sh
@@ -2,4 +2,11 @@
 
 ls ${MAIL_FOLDER}/${MAIL_VERSION}/scripts
 ls ${MAIL_FOLDER}/${MAIL_VERSION}/configuration
+
+IP_ADDRESS=$(getent hosts mail-proxy | awk '{ print $1 }')
+
+sed -i "2s:.*:host = ${IP_ADDRESS}:" ${MAIL_FOLDER}/${MAIL_VERSION}/configuration/config.ini
+
+cat ${MAIL_FOLDER}/${MAIL_VERSION}/configuration/config.ini
+
 python -u ${MAIL_FOLDER}/${MAIL_VERSION}/scripts/mailproxy.py ${MAIL_FOLDER}/${MAIL_VERSION}/configuration/config.ini
