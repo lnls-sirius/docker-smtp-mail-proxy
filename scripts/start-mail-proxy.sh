99e4f36ff3a2f8cfa30c0b71f9778baf99a28a2c
Gustavo Ciotto <gustavociotto@gmail.com>
2018-06-18 14:21:25 -0300

    use 0.0.0.0 instead of specific address

--- scripts/start-mail-proxy.sh
+++ scripts/start-mail-proxy.sh
@@ -3,7 +3,7 @@
 ls ${MAIL_FOLDER}/${MAIL_VERSION}/scripts
 ls ${MAIL_FOLDER}/${MAIL_VERSION}/configuration
 
-IP_ADDRESS=$(getent hosts mail-proxy | awk '{ print $1 }')
+IP_ADDRESS=0.0.0.0
 
 sed -i "2s:.*:host = ${IP_ADDRESS}:" ${MAIL_FOLDER}/${MAIL_VERSION}/configuration/config.ini
 
